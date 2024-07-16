6a024f8a0ab03a57f9d0a54387a51ecf9343f41a
manojkumar <manojkumarr21@gmail.com>
2024-07-16 15:50:35 +0530

    testing

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
 const sendEmail = require('./sendEmail');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-
-
-
 
 const allowedOrigins = ['https://nplustechnologies.com', 'http://192.168.1.17:3000'];
 
@@ -26,40 +22,27 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append extension
-  },
-});
-
+// Set up multer for file uploads using memory storage
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// Ensure the upload directory exists
-const fs = require('fs');
-if (!fs.existsSync('uploads')) {
-  fs.mkdirSync('uploads');
-}
-
 // API endpoint to send email
 app.post('/send-email', upload.single('attachment'), async (req, res) => {
-  const { to, subject ,name,email,phoneNumber,experience } = req.body;
+  const { to, subject, name, email, phoneNumber, experience } = req.body;
   const attachment = req.file;
 
-  if (!to || !subject ||  !attachment) {
+  if (!to || !subject || !attachment) {
     return res.status(400).send('Missing required fields');
   }
 
   try {
-    await sendEmail(to, subject,name,email,phoneNumber,experience,attachment);
+    await sendEmail(to, subject, name, email, phoneNumber, experience, attachment);
     res.status(200).send('Email sent successfully');
   } catch (error) {
+    console.error('Error sending email:', error);
     res.status(500).send('Error sending email');
   }
 });
